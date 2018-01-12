150e405cb51a1821f4b78452d673d9c65f788b63
Nikolay Ninarski <n.ninarski@up2.nl>
2018-01-12 14:17:36 +0200

    Updated initial blog post Added styling of the homepage

---
+++ src/pages/blog/2018-01-12-just-in-small-batch-of-jamaican-blue-mountain-in-store-next-week.md
@@ -0,0 +1,16 @@
+---
+templateKey: 'blog-post'
+path: /hello-again-bird
+title: 'Hello Again, Bird'
+date: 2018-01-12T11:04:10.000Z
+featuredImage: '/img/well-hello-bird.jpg'
+description: >-
+  I haven't blogged for more than a couple of years, but felt like I don't have
+  a spot to save my finding and notes on the things I learn. So here it is.
+---
+
+![Hello again, Bird](/img/well-hello-bird.jpg)
+
+Welcome to my new place for notes and remarks on the things I'm learning. Some of them are beginner's notes and some are the edgiest cases.
+
+I'll also try to bring back some of the content from my old blog (for the sake of the history).
---
+++ src/pages/index.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import Script from 'react-load-script';
+
+export default class IndexPage extends React.Component {
+	handleScriptLoad() {
+		if (window.netlifyIdentity) {
+			window.netlifyIdentity.on('init', user => {
+				if (!user) {
+					window.netlifyIdentity.on('login', () => {
+						document.location.href = '/admin/';
+					});
+				}
+			});
+		}
+		window.netlifyIdentity.init();
+	}
+
+	render() {
+		const { data } = this.props;
+		const { edges: posts } = data.allMarkdownRemark;
+		return (
+			<section className="section">
+				<Script
+					url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+					onLoad={this.handleScriptLoad.bind(this)}
+				/>
+				<div className="container">
+					<div className="content">
+						<h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+					</div>
+					{posts.filter(post => post.node.frontmatter.templateKey === 'blog-post').map(({ node: post }) => {
+
+						let featuredImageUrl = post.frontmatter.featuredImage;
+						let featuredImage
+
+						if( featuredImageUrl ) {
+							featuredImage = (<img className="featured-image" src={ featuredImageUrl } alt={post.frontmatter.title} />)
+						}
+
+						return (
+							<div className="content" style={{ border: '1px solid #eaecee', padding: '2em 4em' }} key={post.id}>
+								<p>
+									<Link className="has-text-primary" to={post.frontmatter.path}>
+										{post.frontmatter.title}
+									</Link>
+									<span> &bull; </span>
+									<small>{post.frontmatter.date}</small>
+								</p>
+								{ featuredImage }
+								<p>
+									<span dangerouslySetInnerHTML={{ __html: post.excerpt }} ></span>
+									<br />
+									<br />
+									<Link className="button is-small is-pulled-right" to={post.frontmatter.path}>
+										Keep Reading →
+									</Link>
+									<div className="is-clearfix"></div>
+								</p>
+							</div>
+						);
+					})}
+				</div>
+			</section>
+		);
+	}
+}
+
+export const pageQuery = graphql`
+	query IndexQuery {
+		allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+			edges {
+				node {
+					excerpt(pruneLength: 400)
+					id
+					frontmatter {
+						title
+						templateKey
+						featuredImage
+						date(formatString: "MMMM DD, YYYY")
+						path
+					}
+				}
+			}
+		}
+	}
+`;
